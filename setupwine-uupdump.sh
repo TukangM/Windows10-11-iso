1b43cb129fdf590a082061ae6923b6a1d8587528
TukangM <bombots23@gmail.com>
2023-11-11 15:13:50 +0000

    ntahlah

--- setupwine-uupdump.sh
+++ setupwine-uupdump.sh
@@ -1,10 +1,12 @@
 #/bin/bash
+sudo apt update
+sudo apt upgrade
 sudo dpkg --add-architecture i386
 sudo mkdir -pm755 /etc/apt/keyrings
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
 sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
 sudo apt update
-sudo apt install --install-recommends winehq-stable
+sudo apt install --install-recommends wine-stable
 
 echo done...
 echo installing noVNC and environtment
@@ -14,6 +16,8 @@ sudo apt -y install xfce4 gnome-software
 echo '#!/bin/bash' > ~/.vnc/xstartup && echo 'xrdb $HOME/.Xresources' >> ~/.vnc/xstartup && echo 'startxfce4 &' >> ~/.vnc/xstartup
 
 echo create vnc password
-vncserver
 
 echo start noVNC
+
+
+echo okay bruh
